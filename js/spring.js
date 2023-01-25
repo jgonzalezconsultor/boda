550e35c0142487f425ab15dc66fa71c979a84d0b
moodle@unila.edu.mx <moodle@unila.edu.mx>
2023-01-24 21:48:49 -0600

    Ajuste de la fotografía y principios del js consumidor de API REST

---
+++ js/spring.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+    var familia = new Object();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/v1/familias",
+        dataType: "json",
+        success:function(data){
+            $.each(data, function(i, item){  
+                familia = item.familia 
+            });
+            Object.values(familia).forEach(val => {
+                var optionFamilia = "<option value='" + val.id + "'>" + val.nombre + "</option>";
+                $("#familias").append(optionFamilia);
+            });
+        }
+    });    
+});
+
+function definirInvitados(element){
+    var id = $('#familias').find(":selected").val();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/v1/familias/" + id,
+        dataType: "json",
+        success:function(data){
+            $.each(data, function(i, item){  
+                familia = item.familia 
+            });
+            Object.values(familia).forEach(val => {
+                for(i = 0; i< val.numeroDeAdultos; i++){
+                    console.log("Adulto " + i);
+                }
+            });
+
+            
+        }
+    });
+}
